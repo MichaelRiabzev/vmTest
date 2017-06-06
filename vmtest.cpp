97e9242c16578c478ce0207b0406f277b488c692
Michael Riabzev <RiabzevMichael@gmail.com>
2017-06-06 12:45:28 +0300

    wrote the code

--- vmtest.cpp
+++ vmtest.cpp
@@ -23,12 +23,11 @@ int main(){
     const long long int GB_size = 1UL<<30;
 
     char sum = 0;
+    std::vector<std::vector<char>> allVecs;
     for(int i=1; i<200; i++){
         std::cout<<"Alocating "<<i<<"GB"<<std::endl;
-        std::vector<char> vec(i*GB_size,5);
-        
-        //just to prevent compiler optimization from eliminating the allocation
-        sum += vec[rand()%vec.size()];
+        std::vector<char> vec(GB_size,5);
+        allVecs.push_back(vec);
 
         std::cout<<"$>free -g"<<std::endl<<exec("free -g")<<std::endl;
     }
